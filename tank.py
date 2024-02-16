921b7c524fdfb880f2a4cfe95ed9640a81a45bba
Rafael Santos <rdss.snf20@uea.edu.br>
2024-02-16 14:50:22 -0400

    adding class tank on file tank.py

---
+++ tank.py
@@ -0,0 +1,7 @@
+class Tank:
+    def __init__(self, posx, posy, colour, width, height):
+        self.posx = posx
+        self.posy = posy
+        self.colour = colour
+        self.width = width
+        self.height = height
