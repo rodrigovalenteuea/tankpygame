339a748b4929a7c2866cd9d671a798d22270bb7d
Rafael Santos <rdss.snf20@uea.edu.br>
2024-02-16 19:11:09 -0400

    update the main menu

--- game.py
+++ game.py
@@ -10,33 +10,38 @@ obs = obstacles.Obstacles(500, 400, 100, 100)
 timer = pygame.time.Clock()
 fps = 60
 screen_setups = [pygame.RESIZABLE, pygame.FULLSCREEN]
+screen_options = 0
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 info = pygame.display.Info()
 screen_width, screen_height = info.current_w, info.current_h
-menu_screen = pygame.display.set_mode((screen_width - 100, screen_height - 100), screen_setups[0])
+menu_screen = pygame.display.set_mode((screen_width - 100, screen_height - 100), screen_setups[screen_options])
 pygame.display.set_caption('Tankpygame ta ligado!')
-
 
 def start_game():
 
-    global menu_screen, obs
+    global menu_screen, obs, screen_options
     running = True
 
     while running:
         pygame.display.update()
         timer.tick(fps)
         menu_screen.fill((0, 0, 0))
-        pygame.draw.rect(menu_screen, 'green', pygame.rect.Rect(obs.posx, obs.posy, obs.width, obs.height))
         menu.draw_main_menu(menu_screen)
-
         key = pygame.key.get_pressed()
         if key[pygame.K_ESCAPE]:
             running = False
+        if key[pygame.K_F11]:
+            if screen_options == 0:
+                screen_options = 1
+                pygame.display.set_mode((0, 0), screen_setups[screen_options])
+            elif screen_options == 1:
+                screen_options = 0
+                pygame.display.set_mode((screen_width - 100, screen_height - 100), screen_setups[screen_options])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+    print(pygame.font.get_fonts())
     pygame.quit()
 
 
--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
 import pygame
+
 pygame.init()
 
 font = pygame.font.Font('freesansbold.ttf', 24)
 def draw_main_menu(screen):
-    pygame.draw.rect(screen, (255, 0, 0), [200, 300, 260, 40])
+    start_btn = pygame.draw.rect(screen, (255, 0, 0), [200, 300, 260, 40])
     menu_text = font.render('Start Game', True, (200, 200, 200))
-    screen.blit(menu_text, (207, 305))
+    screen.blit(menu_text, (start_btn.x + 80, start_btn.y))
