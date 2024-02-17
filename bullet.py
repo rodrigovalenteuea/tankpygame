0ec1f076881c783af994791c75f6b522b96f1ee6
Rafael Santos <rdss.snf20@uea.edu.br>
2024-02-17 17:26:44 -0400

    adding moviment functions to bullet.py

--- bullet.py
+++ bullet.py
@@ -1,3 +1,25 @@
 class Bullet:
-    def __init__(self, speed):
-        self.speed = speed
+    def __init__(self, tank, speedx, speedy, posx, posy, width, height):
+        self.tank = tank
+        self.speedx = speedx
+        self.speedy = speedy
+        self.posx = posx
+        self.posy = posy
+        self.width = width
+        self.height = height
+
+    def moviment_right(self, posx, speedx):
+        self.posx = posx + speedx
+        return posx
+
+    def moviment_left(self, posx, speedx):
+        self.posx = posx - speedx
+        return posx
+
+    def moviment_up(self, posy, speedy):
+        self.posy = posy - speedy
+        return posy
+
+    def moviment_down(self, posy, speedy):
+        self.posy = posy + speedy
+        return posy
