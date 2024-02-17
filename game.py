d2926d0a6ae99657acc1e97c0663bce187def4fb
Rafael Santos <rdss.snf20@uea.edu.br>
2024-02-17 18:04:45 -0400

    functions of bullet and tank is ok

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 import obstacles
-import menu
+import bullet
 import tank
 
 pygame.init()
@@ -10,39 +10,34 @@ main_menu = True
 obs = obstacles.Obstacles(500, 400, 100, 100)
 timer = pygame.time.Clock()
 fps = 60
-screen_setups = [pygame.RESIZABLE, pygame.FULLSCREEN]
-screen_options = 0
+
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 info = pygame.display.Info()
 screen_width, screen_height = info.current_w, info.current_h
-menu_screen = pygame.display.set_mode((screen_width - 100, screen_height - 100), screen_setups[screen_options])
+menu_screen = pygame.display.set_mode((screen_width - 200, screen_height - 200), pygame.RESIZABLE)
 pygame.display.set_caption('Tankpygame ta ligado!')
 
+# test's variables
+tank1 = tank.Tank(screen_width - 400, screen_height/2 + 100, 'green', 100, 100)
+bullet1 = bullet.Bullet(tank1, 'red', 5, 5, tank1.posx + 100, tank1.posy + 100, 20, 20)
 
 def start_game():
 
-    global menu_screen, obs, screen_options
+    global menu_screen, obs
     running = True
 
     while running:
         pygame.display.update()
         timer.tick(fps)
         menu_screen.fill((0, 0, 0))
-        #menu.draw_main_menu(menu_screen)
+
+        tank1.create_tank(menu_screen)
+        tank1.shot(bullet1, menu_screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            '''
-            if event.type == pygame.KEYDOWN and pygame.K_f:
-                if screen_options == 0:
-                    screen_options = 1
-                    pygame.display.set_mode((0, 0), screen_setups[screen_options])
-                elif screen_options == 1:
-                    print(pygame.event.get())
-                    screen_options = 0
-                    pygame.display.set_mode((screen_width - 100, screen_height - 100), screen_setups[screen_options])
-            '''
+
     pygame.quit()
 
 
